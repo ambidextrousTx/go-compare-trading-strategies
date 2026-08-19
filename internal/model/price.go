a84c84deb28417f17fedce8e1d4239983ba0d965
Ravi Sinha <ravisinha@my.unt.edu>
2026-08-19 18:51:34 -0500

    Add price model

---
+++ internal/model/price.go
@@ -0,0 +1,13 @@
+// internal/model/price.go
+package model
+
+import "time"
+
+type PricePoint struct {
+	Date   time.Time
+	Open   float64
+	High   float64
+	Low    float64
+	Close  float64
+	Volume int64
+}
